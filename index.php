ad2d637408dff303437f6a276460c0aab9f0db74
mluecke@gmail.com <7458223@student.swin.edu.au>
2012-08-29 13:52:07 +1000

    Initial Commit

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+<title>Homepage: Reflective Symphony</title>
+
+<link rel="stylesheet" type="text/css" href="styles/v1style.css" />
+
+<meta name="description" content="Reflective Symphony: Personal Website and Portfolio of Michael Luecke" />
+<meta name="keywords" content="Reflective,portfolio,graphic,graphics,Symphony,Michael,Luecke,Melbourne,Photoshop,web,designer,developer,javascript,css,html" />
+<meta name="author" content="Michael Luecke" />
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+
+
+<link rel="icon" href="images/v1layout/favicon.ico" type="image/x-icon" />
+
+</head>
+
+<body>
+
+<div id="outline">
+<div id="inside">
+<div id="insider">
+<img id="topspotlight" src="images/v1layout/topspotlight.png" alt ="topspotlight"/>
+<div id="spotlight">
+<a href="portfolio/portfolio.html#sots" ><img id="spotlightimage" src="images/spotlight/sotssl.png" alt ="Spotlight Image" /></a>
+<a href="restricted/restricted.html"><img id="restricted" width="58" height="58" src ="images/v1layout/restrictedaccess.png" alt ="Pssh Don't tell anyone"/></a>
+</div>
+<img id="bottomspotlight" src="images/v1layout/bottomspotlight.png" alt ="bottomspotlight" />
+
+<img id="header" src="images/v1layout/header.png" alt="Header" />
+
+<div id="homepage" style="height:31px;width:122px;background:url(images/v1layout/activelink.png)" ><a href="index.html"> &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Homepage&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160; </a></div>
+<div id="about" style="height:31px;width:122px;background:url(images/v1layout/inactivelink.png)"> <a href="about/about.html">&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;About&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160; </a></div>
+<div id="portfolio" style="height:31px;width:122px;background:url(images/v1layout/inactivelink.png)"><a href="portfolio/portfolio.html"> &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Portfolio&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;</a> </div>
+<div id="development" style="height:31px;width:122px;background:url(images/v1layout/inactivelink.png)"><a href="development/development.html"> &#160;&#160;&#160;&#160;&#160;&#160;Development&#160;&#160;&#160;&#160; </a> </div>
+<div id="contact" style="height:31px;width:122px;background:url(images/v1layout/inactivelink.png)"> <a href="contact/contact.html">&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Contact&#160;&#160;&#160;&#160;&#160;&#160;</a> </div>
+
+
+<img id="topcontent" src="images/v1layout/topcontent.png" alt ="TopContent"/>
+
+<div id="content">
+<h1> Update for PHP </h1>
+<hr />
+<p> 
+Date: 19/08/2012<br />
+Subject Hey Hey
+<br />
+<br />
+If this works my update was successful... Good work Michael!  That is really productive of you... :P<br /> <br /> <br />
+
+<h1> Recent News </h1>
+<hr />
+<p> 
+Date: 08/07/2011<br />
+Subject Welcome to Reflective Symphony
+<br />
+<br />
+Welcome to Reflective Symphony.  After months of planning I have finally decided to open Reflective Symphony.  The aim of this website is to practice techniques in web design and development, as well as having a bit of fun.  For the time being though, feel free to like us on facebook to keep updated on what is happening with the site.<br/><br/>
+As for now feel free to look around and look for the "hidden"... okay badly hidden restricted area!<br /> <br /> <br />
+
+Please note: There may be issues with a few different browsers, I just wanted to have something up for the time being and will sort these issues out slowly throughout the next month or so, as well as fixing up the badly done html and css. I have only tested it in Firefox and IE for now.  So I know those two browsers work, although there are issues in chrome.  As I still need to put some effort into learning javascript and so forth before the next semester of university starts, these issues will be sorted out slowly as mentioned earlier.  Sorry for the inconvience!!!<br/><br/><br/></p>
+
+<iframe src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fpages%2FReflective-Symphony%2F133062066776462&amp;width=800&amp;colorscheme=light&amp;show_faces=false&amp;border_color&amp;stream=true&amp;header=false&amp;height=395" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:800px; height:395px;" allowTransparency="true"></iframe>
+
+</div>
+<img id="bottomcontent" src="images/v1layout/bottomcontent.png" alt="BottomContent"/>
+</div></div></div>
+
+</body>
+
+</html>
